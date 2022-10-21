f72b47bdad4fe51ba45b917896eaa94e52e954f9
Manwoo Yi <me@navilera.com>
2022-10-21 00:45:20 -0700

    NavilIME for Mac posting

--- cp_image.sh
+++ cp_image.sh
@@ -1,16 +1,17 @@
 YR=`date +%Y`
-DEST=assets/images/${YR}/
+DEST=static/images/${YR}/
+LINK=images/${YR}/
 echo Copy $1 to ${DEST} 
 mkdir -p ${DEST}
 if [ -f "$1" ]; then
     echo $1 existe
-    HASH=`sha256sum $1 | awk '{print $1}'`
+    HASH=`shasum $1 | awk '{print $1}'`
     echo HASH : ${HASH}
     EXTENSION=${1##*.}
     TARGET=${HASH}.${EXTENSION}
     echo cp $1 ${DEST}${TARGET}
     cp  -f $1 ${DEST}${TARGET}
-    echo "![](/"${DEST}${TARGET}")"
+    echo "![](/"${LINK}${TARGET}")"
 else
     echo $1 does not exist
 fi
